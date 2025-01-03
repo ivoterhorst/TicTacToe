5f86c0cf095a1f5a4139d509cf341a2d15d6cccc
Ivo ter Horst <ivoterhorst@hotmail.com>
2025-01-03 23:08:59 +0100

    Tests running

--- lib/board.hpp
+++ lib/board.hpp
@@ -1,18 +1,23 @@
 #ifndef BOARD_HPP
 #define BOARD_HPP
 
-class Board {
+#include <vector>
+
+class Board
+{
 public:
     Board();
     bool makeMove(int row, int col, char player);
     bool checkWin(char player) const;
     bool isFull() const;
+    void display() const;
 
 private:
-    char board[3][3];
-    bool checkRow(int row) const;
-    bool checkCol(int col) const;
-    bool checkDiagonals() const;
+    std::vector<std::vector<char>> board;
+
+    bool checkRow(int row, char player) const;
+    bool checkCol(int col, char player) const;
+    bool checkDiagonals(char player) const;
 };
 
 #endif // BOARD_HPP
--- tests/test_board.cpp
+++ tests/test_board.cpp
@@ -2,8 +2,27 @@
 
 #include "lib/board.hpp"
 
-TEST(EmptyBoard, BasicAssertions) {
+TEST(BoardAssertions, EmptyBoard)
+{
   // new board should be empty
-    Board board;
-    EXPECT_FALSE(board.checkWin('X'));
+  Board board;
+  EXPECT_FALSE(board.checkWin('X'));
+}
+
+TEST(BoardAssertions, EmptyBoard_FirstMove)
+{
+  // new board should be empty
+  Board board;
+  board.makeMove(0, 0, 'X');
+  EXPECT_FALSE(board.checkWin('X'));
+}
+
+TEST(BoardAssertions, EmptyBoard_FirstRow)
+{
+  // new board should be empty
+  Board board;
+  board.makeMove(0, 0, 'X');
+  board.makeMove(0, 1, 'X');
+  board.makeMove(0, 2, 'X');
+  EXPECT_TRUE(board.checkWin('X'));
 }
